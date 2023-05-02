adb1c57b6bae52c5782fbaa2f673e00aa971bd3c
kimschles <schlesinger.kim@gmail.com>
2023-05-01 19:49:04 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+function sayHello() {
+    // let name = args.name || 'stranger'
+    let greeting = 'Hi David!\n' + '--Kim 😀'
+    console.log(greeting)
+    return { "body": greeting }
+}
+
+sayHello()
